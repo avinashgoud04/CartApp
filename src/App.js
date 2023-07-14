79d08cb3ac86852025803e89a24bbf9ec910203b
Avinash <ananthula.avinash@go-mmt.com>
2023-07-14 17:18:35 +0530

    Initial commit with following features:
    - Lets user add item to cart.
    - user can manage items from cart.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from "react";
+import "./App.css";
+import cartReducer from "./reducers/cartReducer";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
 
 function App() {
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: [],
+    cart: [],
+  });
+
+  const fetchProducts = async () => {
+    const data = await fetch("https://dummyjson.com/products");
+    const jsonData = await data.json();
+
+    dispatch({
+      type: "ADD_PRODUCTS",
+      payload: jsonData.products,
+    });
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: "flex"}}>
+      <Products state={state} dispatch={dispatch}/>
+      <Cart state={state} dispatch={dispatch}/>
     </div>
   );
 }
