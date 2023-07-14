c1ca672dfa67e1bf585bce6f66174208aa40ca0e
Avinash <ananthula.avinash@go-mmt.com>
2023-07-14 17:51:57 +0530

    Added error handling ( if api is failed )

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 import "./App.css";
 import cartReducer from "./reducers/cartReducer";
 import Products from "./components/Products";
@@ -9,24 +9,38 @@ function App() {
     products: [],
     cart: [],
   });
+  const [isError, setIsError] = useState(false);
 
   const fetchProducts = async () => {
-    const data = await fetch("https://dummyjson.com/products");
-    const jsonData = await data.json();
-
-    dispatch({
-      type: "ADD_PRODUCTS",
-      payload: jsonData.products,
-    });
+    try {
+      const data = await fetch("https://dummyjson.com/products");
+      const jsonData = await data.json();
+      dispatch({
+        type: "ADD_PRODUCTS",
+        payload: jsonData.products,
+      });
+    } catch (error) {
+      setIsError(true);
+      console.error("Something went wrong while fetching data: ",error);
+    }
   };
 
   useEffect(() => {
     fetchProducts();
   }, []);
+
   return (
-    <div style={{display: "flex"}}>
-      <Products state={state} dispatch={dispatch}/>
-      <Cart state={state} dispatch={dispatch}/>
+    <div style={{ display: "flex" }}>
+      {state.products.length > 0 ? (
+        <>
+          <Products state={state} dispatch={dispatch} />
+          <Cart state={state} dispatch={dispatch} />
+        </>
+      ) : (
+        isError ?
+        <p style={{alignSelf: "center"}}>{'Error while fetching data :('}</p>:
+        <p style={{alignSelf: "center"}}>{'No Products Available currently'}</p>
+      )}
     </div>
   );
 }
